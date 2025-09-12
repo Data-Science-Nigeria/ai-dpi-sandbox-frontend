e31cfc49376df2a25a94bf5b3227bd5cdeb1a01a
Pharza07 <belloemmanuel308@gmail.com>
2025-09-12 10:53:54 +0100

    fix: code change

--- lib/env.ts
+++ lib/env.ts
@@ -1,3 +1,3 @@
 export const getBaseUrl = (): string => {
-  return process.env.NEXT_PUBLIC_API_URL || "http://dsnsandbox.com";
+  return process.env.NEXT_PUBLIC_API_URL || "http://dsnsandbox.com:8080";
 };
